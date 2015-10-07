390ea03bb5c6a44bdd8df14d6b568841cd7e109e
Janelle <blanellesworld@gmail.com>
2015-10-07 15:15:51 -0400

    Updating urls and views for new post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post 
 from .forms import PostForm
@@ -14,21 +14,7 @@ def post_detail(request, pk):
 
 def post_new(request):
 	if request.method == "POST":
-		form = PostForm(REQUEST.POST)
-		if form.is_valid():
-			post= form.save(commit=False)
-			post.author = request.user
-			post.published_date = timezone.now()
-			post.save
-			return redirect('blog.views.post_detail', pk=post.pk)
-	else:
-		form = PostForm()
-	return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_edit(request, pk):
-	post = get_object_or_404(Post, pk=pk)
-	if request.method == "POST":
-		form = PostForm(request.POST, instance=post)
+		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
@@ -36,5 +22,19 @@ def post_edit(request, pk):
 			post.save()
 			return redirect('blog.views.post_detail', pk=post.pk)
 	else:
-		form = PostForm(instance=post)
-	return render(request, 'blog/post_edit.html', {'form':form})
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+        else:
+        	form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form':form})
